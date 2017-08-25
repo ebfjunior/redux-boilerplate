0e72eb53e9b8a8237f159d9ae86b699a24fc3778
Edson Júnior <ejunior.batista@gmail.com>
2017-08-25 14:33:12 -0300

    Fixed path for reducers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import reducers from './reducers';
+import reducers from './reducers/root';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
